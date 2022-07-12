4549c92e271beab1152c4ab5362a20a95a1a55a7
Ilya <sijonra@gmail.com>
2022-07-12 19:19:13 +0300

    added basic input handler

--- src/components/ToDoInput/InputForm.tsx
+++ src/components/ToDoInput/InputForm.tsx
@@ -1,15 +1,41 @@
 import style from './InputForm.module.scss'
-import React from "react";
+import React, {useRef, useState} from "react";
 
-const InputForm: React.FC = () =>{
+interface InputFormProps{
+    addNewToDoItem(item: string): void
+}
+
+const InputForm: React.FC<InputFormProps> = props =>{
+
+    let ref = useRef<HTMLInputElement>(null)
+    let [inputValue, setInputValue] = useState<string>('');
+
+    let changeInputValue = () =>{
+        setInputValue(ref.current!.value)
+    }
+
+    let inputSubmit = (event: React.FormEvent) =>{
+        event.preventDefault()
+        props.addNewToDoItem(inputValue)
+    }
+
     return(
-        <form className={style.inputForm}>
+        <form
+            className={style.inputForm}
+            onSubmit={inputSubmit}
+        >
             <input
                 type="text"
                 className={style.input}
+                value={inputValue}
                 placeholder={'some words'}
+                onChange={changeInputValue}
+                ref={ref}
             />
-            <button className={style.submit}>
+            <button
+                className={style.submit}
+                type={"submit"}
+            >
                 <svg width="26" height="25" viewBox="0 0 26 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M7.04948 12.5955H19.3224" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     <path d="M13.1859 18.5955L13.1859 6.59552" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import style from  './ToDoList.module.scss';
 import InputForm from "../ToDoInput/InputForm";
 import ToDoItem from "../ToDoItem/ToDoItem";
 
 const ToDoList: React.FC = () =>{
 
+    let [toDoItems, setToDoItems] = useState([]);
+
+    let addNewToDoItem = (item:string) =>{
+        console.log(item);
+    }
+
     return(
         <section className={style.ToDoList}>
             <h2 className={style.title}>To Do List</h2>
-            <InputForm />
+            <InputForm addNewToDoItem={addNewToDoItem}/>
             <div className={style.toDoItems}>
                 <ToDoItem />
             </div>
